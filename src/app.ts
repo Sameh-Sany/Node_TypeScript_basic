da74ae9ce366a5280a5facc0ce50af939cd4d4b4
Dev-Sameh <92803501+Sameh-Sany@users.noreply.github.com>
2023-08-22 02:45:29 +0300

    add todo crud

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,17 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+
+import todosRoutes from "./routes/todos";
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/todos", todosRoutes);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({
+    message: error.message,
+  });
+});
 
 app.listen(3000);
